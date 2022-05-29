22d7c942008ee0fb48f78549de6630200a043e86
Miguel Fialho <fialhomig@gmail.com>
2022-05-29 12:03:04 -0300

    Finalizando modulos

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,1 +1,4 @@
 "use strict";
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
+// const PI = 2.99
---
+++ namespaces/namespaces.ts
@@ -0,0 +1,3 @@
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
+// const PI = 2.99
